560a47e8999456d2464ca0788ab20405949947a2
fernando.silva <fernandodiassilva96@hotmail.com>
2025-03-21 08:43:06 -0300

    Commit inicial do projeto

---
+++ main.tf
@@ -0,0 +1,94 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_db_subnet_group" "my_db_subnet_group" {
+  name       = "my-db-subnet-group"
+  subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+
+  tags = {
+    Name = "My DB Subnet Group"
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-security-group"
+  description = "Security group for RDS"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Altere para IPs mais restritos por segurança
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami             = "ami-04b4f1a9cf54c11d0"
+  instance_type   = "t3.micro"
+  key_name        = "iac-alura"
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  subnet_id       = aws_subnet.subnet_a.id
+
+  tags = {
+    Name = "TF - EC2"
+  }
+}
+
+variable "db_password" {}
+
+resource "aws_db_instance" "default" {
+  identifier = "tf-postgres"
+  allocated_storage    = 10
+  db_name              = "mydatabase"
+  engine               = "postgres"
+  engine_version       = "17.4"
+  instance_class       = "db.t4g.micro"
+  username             = "root"
+  password             = var.db_password
+  parameter_group_name = "default.postgres17"
+  skip_final_snapshot  = true
+
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.my_db_subnet_group.name
+
+  tags = {
+    Name = "TF - RDS"
+  }
+}
